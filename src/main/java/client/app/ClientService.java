4746cab6411796a51f964b450f487673ff3f7ba0
jix111 <1193727535@qq.com>
2023-09-04 16:57:04 +0800

    revert last commit

--- src/main/java/client/app/ClientService.java
+++ src/main/java/client/app/ClientService.java
@@ -5,6 +5,5 @@ import client.ui.LoginFrame;
 public class ClientService {
     public static void main(String[] args) {
         LoginFrame.getInstance();
-        System.out.println("Client Started!");
     }
 }
--- src/main/java/server/app/ServerService.java
+++ src/main/java/server/app/ServerService.java
@@ -7,6 +7,5 @@ public class ServerService {
     public static void main(String[] args) {
         Config.init();
         new Server();
-        System.out.println("Server Started!");
     }
 }
